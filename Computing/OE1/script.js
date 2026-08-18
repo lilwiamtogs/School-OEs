08dc77c1fd9ae5bbd60c81e2520def111c5610b0
William <williamtogonon@gmail.com>
2026-08-18 14:21:46 +0800

    Complete OE1 website

---
+++ Computing/OE1/script.js
@@ -0,0 +1,9 @@
+function showMessage(){
+
+    alert(
+        "Welcome to My First Website!\n\n" +
+        "Thank you for visiting our webpage.\n\n" +
+        "This webpage was created using HTML, CSS, and JavaScript."
+    );
+
+}
