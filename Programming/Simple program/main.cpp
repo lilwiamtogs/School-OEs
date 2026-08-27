36da81d112dca4228d2f5ad63cec1932050c4d73
William <williamtogonon@gmail.com>
2026-08-27 14:45:10 +0800

    new oes

---
+++ Programming/Simple program/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    string playerName;
+    cout << "Enter your player name: ";
+    getline(cin, playerName);
+
+    string playerClass;
+    cout << "Enter your player class (e.g., Warrior, Mage, Rogue): ";
+    getline(cin, playerClass);
+
+    bool hasMount;
+    cout << "Does your player have a mount? (1 for yes, 0 for no): ";
+    cin >> hasMount;
+
+    cout << boolalpha;
+    cout << "\n_________________________________\n\n";
+    cout << "Player Name: " << playerName << endl;
+    cout << "Player Class: " << playerClass << endl;
+    cout << "Has Mount: " << hasMount << endl;
+    cout << "\n_________________________________\n";
+}
